864ff24345b475be4421526fcf2a0229f9426daf
dabai23 <109806163+dabai23@users.noreply.github.com>
2023-06-07 19:04:41 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 	/* 当HTML文档加载完成后执行函数 */ var qrCode1Image = new Image(); /* 创建一个Image对象用于加载QR Code图像 */
 	qrCode1Image.src = new QRCode(document.createElement("div") /* 创建一个div元素 */, {
-		text: "https://qr.alipay.com/tsx12297kmehivtyqnfjz42" /* 设置二维码1的内容 */,
+		text: "https://qr.alipay.com/fkx10943hijbxfjcw73d0fd" /* 设置二维码1的内容 */,
 		width: 300 /* 设置二维码宽度 */,
 		height: 300 /* 设置二维码高度 */,
 		correctLevel: QRCode.CorrectLevel.H /* 设置二维码纠错级别 */,
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	var qrCode3Image = new Image(); /* 创建一个Image对象用于加载QR Code图像 */
 	qrCode3Image.src = new QRCode(document.createElement("div") /* 创建一个div元素 */, {
-		text: "https://i.qianbao.qq.com/wallet/sqrcode.htm?m=tenpay&f=wallet&a=1&ac=CAEQyrOIiQsYnbT2owZCIDQyNWQxNTc1YzA1ZDc1ZDMyYWU1NzI3N2MxZjE1MTNi_xxx_sign&u=2971802058&n=%E7%A2%8E%E5%BF%B5%F0%9F%8D%82" /* 设置二维码3的内容 */,
+		text: "https://i.qianbao.qq.com/wallet/sqrcode.htm?m=tenpay&f=wallet&a=1&ac=CAEQ4PHLcxj7yf2jBkIgOTQ3ZTZjYWMwNjVmMzZjNTIyYzY2ZmY5MTczZmIxMTU%3D_xxx_sign&u=242415840&n=%E5%A4%A7%E7%99%BD" /* 设置二维码3的内容 */,
 		width: 300 /* 设置二维码宽度 */,
 		height: 300 /* 设置二维码高度 */,
 		correctLevel: QRCode.CorrectLevel.H /* 设置二维码纠错级别 */,
